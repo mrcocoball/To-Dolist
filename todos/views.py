ed5e7ff66e3e42e94ba7b0dcc14fa465c3c5dbaa
mrcocoball <gunmaru93@gmail.com>
2023-03-24 14:44:44 +0900

    #28 - 동적 검색 기능 구현 및 검색 로직 변경
    * 자신이 작성한 ToDo만 볼 수 있도록 로직 변경
    * 동적 검색 기능 (제목/본문 검색, 일자 지정) 구현

--- todos/views.py
+++ todos/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.views.decorators.http import require_POST
-from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from todos.models import Todo
 from todolist.views import OwnerOnlyMixin
+from django.db.models.query_utils import Q
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from datetime import datetime, timedelta
 
 # ListView
 class TodoLV(ListView):
@@ -17,6 +19,66 @@ class TodoLV(ListView):
     paginate_by = 10
     template_name = 'todos.html'
 
+    def get_queryset(self):
+
+        # 동적 쿼리
+
+        search_type = self.request.GET.get("searchType", "")
+        keyword = self.request.GET.get("keyword", "")
+        start_date = self.request.GET.get("startDate", "")
+        target_date = self.request.GET.get("targetDate", "")
+
+        q = Q()
+
+        if search_type:
+            if search_type == "title": # 제목 검색
+                if keyword:
+                    q.add(Q(title__contains=keyword), q.AND)
+                if start_date:
+                    if target_date:
+                        convert_date = datetime.strptime(target_date, "%Y-%m-%d").date() + timedelta(days=1) # target_date에 하루 추가
+                        q.add(Q(created_at__gte=start_date, created_at__lte=convert_date), q.AND)
+                    else:
+                        q.add(Q(created_at__gte=start_date), q.AND)
+                else:
+                    if target_date:
+                        convert_date = datetime.strptime(target_date, "%Y-%m-%d").date() + timedelta(days=1) # target_date에 하루 추가
+                        q.add(Q(created_at__lte=convert_date), q.AND)
+            if search_type == "description": # 본문 검색
+                if keyword:
+                    q.add(Q(description__contains=keyword), q.AND)
+                if start_date:
+                    if target_date:
+                        convert_date = datetime.strptime(target_date, "%Y-%m-%d").date() + timedelta(days=1) # target_date에 하루 추가
+                        q.add(Q(created_at__gte=start_date, created_at__lte=convert_date), q.AND)
+                    else:
+                        q.add(Q(created_at__gte=start_date), q.AND)
+                else:
+                    if target_date:
+                        convert_date = datetime.strptime(target_date, "%Y-%m-%d").date() + timedelta(days=1) # target_date에 하루 추가
+                        q.add(Q(created_at__lte=convert_date), q.AND)
+
+        if self.request.user.is_authenticated:
+            return Todo.objects.filter(owner=self.request.user).filter(q)
+        else:
+            return [] # AnonymousUser는 빈 리스트 노출
+    
+        
+    def get_context_data(self, **kwargs):
+        search_type = self.request.GET.get("searchType", "")
+        keyword = self.request.GET.get("keyword", "")
+        start_date = self.request.GET.get("startDate", "")
+        target_date = self.request.GET.get("targetDate", "")
+
+        context = super().get_context_data(**kwargs)
+
+        context['st'] = search_type
+        context['k'] = keyword
+        context['s'] = start_date
+        context['t'] = target_date
+
+        return context
+    
 
 # DetailView
 class TodoDV(DetailView):
