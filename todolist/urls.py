e6f71612c8b7b24d0447b387c8cb5c8d7c178b92
mrcocoball <gunmaru93@gmail.com>
2023-03-28 21:51:22 +0900

    #37 - Static 경로 관련 처리
    nginx 관련 정적 파일에 대한 re_path 관련 작업을 처리하였음

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +10,8 @@ urlpatterns = [
     path('', include('accounts.urls')),
     path('profiles/', include('profiles.urls')),
 ]
+
+if settings.DEBUG is False:
+    urlpatterns += [
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
+    ]
